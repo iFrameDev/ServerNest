d0a9f12deca2124323d5c878f38861ae89334112
iFrame <iframedev@gmail.com>
2023-03-13 18:49:39 +0100

    add module mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/SocialLifeWebApp'),UserModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/SocialLifeWebApp'),UserModule, AuthModule, MailModule],
   controllers: [AppController],
   providers: [AppService]
 
